e8cb787ef7528b183ab35079d9c8843325e6a424
Alan Paiva <jpaiva.alan@gmail.com>
2021-11-27 19:20:27 -0300

    Clean up code

--- assignment_1.py
+++ assignment_1.py
@@ -6,12 +6,7 @@ from unconstrained_optimization.descent_methods import SteepestDescent, STEEPEST
 from unconstrained_optimization.descent_methods import NewtonStep
 from unconstrained_optimization.plot_helper import plot_error_curve, plot_levels
 
-# TODO: Remove seed
-# TODO: Grid Search for Hyper params
-
 # Define starting point for the optimization
-np.random.seed(11)
-# x0 = np.random.uniform(low=-4, high=0, size=(2,))
 x0 = np.array([1, 1])
 epsilon = 1e-7
 
@@ -21,7 +16,7 @@ f = Function()
 # Define line search method
 # line_search = ConstantLineSearch(t=0.01)
 # line_search = BacktrackingLineSearch(f=f, alpha=0.3, beta=0.7)
-line_search = ExactLineSearch(f=f)
+# line_search = ExactLineSearch(f=f)
 
 # Gradient Descent
 # optimizer = GradientDescent(f=f, line_search=line_search, epsilon=epsilon)
